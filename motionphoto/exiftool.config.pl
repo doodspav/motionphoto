f505ce88de5531bc992518d74e1a46855bc33ff2
doodspav <doodspav@gmail.com>
2024-04-17 23:21:13 +0100

    Add ExifTool config file for compatibility with older (and current) versions

---
+++ motionphoto/exiftool.config.pl
@@ -0,0 +1,19 @@
+%Image::ExifTool::UserDefined = (
+
+    # XMP tags for Motion Photo
+    'Image::ExifTool::XMP::GCamera' => {
+
+        # Google
+        MicroVideo                        => { Writable => 'integer' },
+        MicroVideoVersion                 => { Writable => 'integer' },
+        MicroVideoOffset                  => { Writable => 'integer' },
+        MicroVideoPresentationTimestampUs => { Writable => 'integer' },
+
+        # Samsung
+        MotionPhoto =>                        { Writable => 'integer' },
+        MotionPhotoVersion =>                 { Writable => 'integer' },
+        MotionPhotoPresentationTimestampUs => { Writable => 'integer' },
+    },
+);
+
+1;  # end
