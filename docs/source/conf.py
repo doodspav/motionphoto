bf8165df7eb34ed198dffc71f47d43368cc37922
doodspav <doodspav@gmail.com>
2024-04-27 04:47:08 +0100

    GHI #4 Improve conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,26 +3,34 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import pathlib
+import sys
+
+MOTIONPHOTO_PATH = pathlib.Path(__file__).parent.parent.parent
+sys.path.insert(0, str(MOTIONPHOTO_PATH))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'motionphoto'
-copyright = '2024, doodspav'
-author = 'doodspav'
-release = '0.1.2'
+project = "motionphoto"
+copyright = "2024, doodspav"
+author = "doodspav"
+release = "0.1.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
